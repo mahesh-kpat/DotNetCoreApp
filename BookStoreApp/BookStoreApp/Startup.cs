fc48f2293078a981edfd8aeb960357c123855554
Mahesh <maheshpattnayak91@gmail.com>
2023-07-16 14:26:11 +0530

    change in startup.cs

--- BookStoreApp/BookStoreApp/Startup.cs
+++ BookStoreApp/BookStoreApp/Startup.cs
@@ -32,7 +32,7 @@ namespace BookStoreApp
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World! mahesh bhai.. donot stop learning..");
                 });
             });
         }

a751c35b30087146f50535185643d412adb469d5
Mahesh <maheshpattnayak91@gmail.com>
2023-10-18 11:47:35 +0530

    Added MVC to application

--- BookStoreApp/BookStoreApp/Startup.cs
+++ BookStoreApp/BookStoreApp/Startup.cs
@@ -16,6 +16,8 @@ namespace BookStoreApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //to use controller and views in this application    
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +32,12 @@ namespace BookStoreApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World! mahesh bhai.. donot stop learning..");
-                });
+                endpoints.MapDefaultControllerRoute();
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("mahesh.");
+                //});
             });
         }
     }
